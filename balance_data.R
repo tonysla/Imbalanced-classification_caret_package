4d2e89fbb9f6b3e827b805522030e6a8dc89ea9c
Elton Islamaj <elton.islamaj@gmail.com>
2017-04-18 13:57:19 -0400

    minor changes
    balanced data

--- balance_data.R
+++ balance_data.R
@@ -28,7 +28,7 @@ str(diabetes)
 
 # over sampling using 'ROSE'
 balanced_data <- ovun.sample(outcome ~ ., data = diabetes, method = "over", 
-                         seed = 1, N = 956)$data
+                             seed = 1, N = 990)$data
 table(balanced_data$outcome)
 # split data into train validation & test set (the ideal way)
 # you can choose to just split the data into train and test set
@@ -42,12 +42,12 @@ cvCont <- trainControl(method = "repeatedcv", number = 5,
                        classProbs = TRUE)
 
 grid <- expand.grid(model = "tree",
-                    trials = 1:100,
+                    trials = c(1:100),
                     winnow = FALSE)
 # set seed for reproducibility
 set.seed(1)
 
-# now let's train the first model
+# train the first model
 c50_model <- train(x = train_x[, -9],
                    y = train_x[, 9],
                    method = "C5.0",
@@ -65,12 +65,12 @@ plot(c50_model, transform.x = log10,
      xlab = expression(log[10](gamma)), 
      ylab = "cost")
 
-# predict test data using trained model
+# predict valid_set using trained model
 valid_set$pred <- predict(c50_model, newdata = valid_set[,-9], type = 'raw')
 confusionMatrix(valid_set$outcome, valid_set$pred)
 
 # plot roc 
-p <- predict(model_one, newdata = valid_set, type = 'prob')[,2]
+p <- predict(c50_model, newdata = valid_set, type = 'prob')[,2]
 plot.roc(valid_set$outcome, p, percent=TRUE, print.auc=TRUE)
 
 # let's train the second model. It shares same trControl
@@ -81,7 +81,6 @@ svm_model <- train(x = train_x[, -9],
                    tuneLength = 3,
                    trControl = cvCont,
                    method = "svmLinearWeights",
-                   #tuneGrid = ex_gr,
                    preProc = c("center", "scale"))
 # check model
 svm_model
@@ -92,13 +91,14 @@ plot(svm_model, transform.x = log10,
      ylab = "cost")
 
 # run valid set again
-valid_set <- data_over[indices == 2,]
+valid_set <- balanced_data[indices == 2,]
 # predict test data using trained model
-valid_set$pred_2nd <- predict(svm_model, newdata = valid_set[,-9], type = 'raw')
+valid_set$pred_2nd <- predict(svm_model, newdata = valid_set[,-9], 
+                              type = 'raw')
 confusionMatrix(valid_set$outcome, valid_set$pred_2nd)
 
 # run valid set again, if not will show an error
-valid_set <- data_over[indices == 2,]
+valid_set <- balanced_data[indices == 2,]
 # plot roc for SVM model_two
 p2 <- predict(svm_model, newdata = valid_set[-9], type = 'prob')[,2]
 plot.roc(valid_set$outcome, p2, percent=TRUE, print.auc=TRUE)
@@ -125,29 +125,24 @@ plot(gbmFit, transform.x = log10,
 valid_set$pred_3rd <- predict(gbmFit, newdata = valid_set[,-9], type = 'raw')
 confusionMatrix(valid_set$outcome, valid_set$pred_3rd)
 
-# plot roc for SVM model_two
+# run valid set again, if not will show an error
+valid_set <- balanced_data[indices == 2,]
+# plot roc for gbm model
 p3 <- predict(gbmFit, newdata = valid_set[-9], type = 'prob')[,2]
 plot.roc(valid_set$outcome, p3, percent=TRUE, print.auc=TRUE)
 
 # test best trained model on the test set.
-# Usually models under perform on test set. 
 test_pred <- predict(c50_model, newdata = test_y[,-9], type = 'raw')
 confusionMatrix(test_y$outcome, test_pred)
+
+# calculating F1 score for the test set
+precision <- posPredValue(test_pred, test_y$outcome)
+recall <- sensitivity(test_pred, test_y$outcome)
+F1 <- (2 * precision * recall) / (precision + recall)
+F1
 
 # check accuracy
 accuracy <- table(Actual = test_y$outcome, Pred = test_pred)
 # accuracy for each group in percentage
 addmargins(round(prop.table(accuracy, 1), 3) * 100)
 
-# check which variables/columns are most important
-varImpact <- varImp(c50_model, scale = FALSE)
-varImpact
-# plot top 20 most important variables
-plot(varImpact, 8, main = "c50 model")
-
-# plot the actual/known result
-plot(test_y$outcome, main = 'Actual')$actual
-# plot matrix of predicted accuracy
-plot(accuracy, main = 'Accuracy')
-# plot predicted from the model
-plot(test_pred, main = 'Predicted')
